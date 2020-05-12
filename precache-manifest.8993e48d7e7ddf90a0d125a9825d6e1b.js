e59e19a9fe6a829e8e2a2f8b4909bb298a800370
Klaudia <klaudia.krzywda@gmail.com>
2020-05-12 12:00:34 +0100

    Updates

---
+++ precache-manifest.8993e48d7e7ddf90a0d125a9825d6e1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575fbe7270a884a9b2c5e96ee26d27e5",
+    "url": "/Videos/index.html"
+  },
+  {
+    "revision": "1d0f1f0d2cbedaed210b",
+    "url": "/Videos/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "d6a9414ba19d0d7f0842",
+    "url": "/Videos/static/js/2.aa820dce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Videos/static/js/2.aa820dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0f1f0d2cbedaed210b",
+    "url": "/Videos/static/js/main.bc431a07.chunk.js"
+  },
+  {
+    "revision": "80bba3140edd64002ecd",
+    "url": "/Videos/static/js/runtime-main.dd210870.js"
+  }
+]);
